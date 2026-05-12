7165a4c9d24498429ee7ebf4cc5cf781ae1bf28f
BuiThanh9935 <robluccibn040@gmail.com>
2026-05-12 08:42:40 +0700

    upload file v3

--- mysqlsampledatabase.sql
+++ mysqlsampledatabase.sql
@@ -219,3 +219,17 @@ CALL SetCounter(@counter,1);
 CALL SetCounter(@counter,1);
 CALL SetCounter(@counter,5);
 SELECT @counter;
+
+-- Thực hành: View trong MySQL
+CREATE VIEW customer_views AS
+SELECT customerNumber, customerName, phone
+FROM  customers;
+
+SELECT * FROM customer_views;
+
+CREATE OR REPLACE VIEW customer_views AS
+SELECT customerNumber, customerName, contactFirstName, contactLastName, phone
+FROM customers
+WHERE city = 'Nantes';
+
+DROP VIEW customer_views;
